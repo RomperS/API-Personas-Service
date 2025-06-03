333ef05fb5602afc4449b6f6f800b59cc1326b1d
RomperS <Oswaldolo663@gmail.com>
2025-06-03 18:52:53 -0500

    feat: initial commit

---
+++ src/main/java/com/oswaldo/apipersonasservice/ApiPersonasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oswaldo.apipersonasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPersonasServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPersonasServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oswaldo/apipersonasservice/ApiPersonasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oswaldo.apipersonasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPersonasServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
